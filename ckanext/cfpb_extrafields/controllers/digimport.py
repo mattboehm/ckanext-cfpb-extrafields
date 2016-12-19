8d668bd491c80580c499e00fde4f388df9ee3caa
Matthew Boehm <boehm.matthew@gmail.com>
2016-12-19 09:10:07 -0500

    Add name to record

--- ckanext/cfpb_extrafields/controllers/digimport.py
+++ ckanext/cfpb_extrafields/controllers/digimport.py
@@ -3,12 +3,16 @@ try:
 except ImportError:
     from stringIO import StringIO
 import json
+import re
 import urllib
 
 from ckan.plugins.toolkit import BaseController, redirect_to, render, request, response
 import ckanapi
 
 from ckanext.cfpb_extrafields.digutils import make_rec
+
+def make_name(title):
+    return re.sub(r"[^a-zA-Z0-9]*", "-", title)
 
 def upload_rec(rec):
     api = ckanapi.LocalCKAN()
@@ -36,6 +40,7 @@ class ImportController(BaseController):
             redirect_to("import_page", errors=json.dumps(errors), group=group)
         else:
             rec["owner_org"] = group
+            rec["name"] = make_name(rec["title"])
             upload_rec(rec)
             import logging;logging.error(repr(rec))
         return json.dumps(rec)
